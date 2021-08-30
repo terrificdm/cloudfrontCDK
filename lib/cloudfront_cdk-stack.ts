acae2065c00f70a0b85f7e804e605f75c7e212ef
terrificdm <62762145+terrificdm@users.noreply.github.com>
2021-08-30 13:38:54 +0800

    Update cloudfront_cdk-stack.ts

--- lib/cloudfront_cdk-stack.ts
+++ lib/cloudfront_cdk-stack.ts
@@ -47,7 +47,7 @@ export class CloudfrontCdkStack extends cdk.Stack {
       vpc: vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: amznLinux,
-      keyName:'demo', // you need to modify the value of keyName with your own key-pairs name!
+      keyName:'demo', // You need to modify the value of keyName with your own key-pairs name!
       role: instance_role
     });
     
